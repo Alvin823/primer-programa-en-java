2f0cfdb8617d446e8a7d38bfc5f30a35685d3407
Alvin <alvin8233@gmail.com>
2021-10-19 10:09:18 +0200

    BLK

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,10 @@ public class Prueba {
 	
 	public static void main (String args[]) {	
 	
-	System.out.println("Alvaro Robles Gonzalez");
+
 	System.out.println("Leon 2002");
 	System.out.println("Leon Trobajo del camino Windows");
+	System.out.println("Blake");
 	
 	
 	
